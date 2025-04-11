f22d1ac26738318469b1d06ff2dd3b95e135e239
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2025-04-11 18:59:13 -0400

    fix(login-page): change name for login page

---
+++ app/routes/login.page.tsx
@@ -0,0 +1,17 @@
+import Login from "~/components/Login/login";
+import type { Route } from "./+types/login";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function LoginPage() {
+  return (
+    <>
+      <Login></Login>
+    </>
+  );
+}
