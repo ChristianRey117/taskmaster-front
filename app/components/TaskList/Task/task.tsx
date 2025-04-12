a4f05861569415f165aa77f20ac6ef986db08f40
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2025-04-11 20:56:42 -0400

    feat(task): add card component to show information of task

--- app/components/TaskList/Task/task.tsx
+++ app/components/TaskList/Task/task.tsx
@@ -1,7 +1,36 @@
-export default function Task() {
+import { Button, Card, CardContent, CardHeader, Grid } from "@mui/material";
+import { useEffect } from "react";
+import type { ITask } from "~/intefaces/ITaskResponse";
+
+export default function Task(task: ITask) {
+  useEffect(() => {}, []);
   return (
     <>
-      <p>Task</p>
+      <Card>
+        <CardHeader title={task.title}></CardHeader>
+        <CardContent>
+          <Grid container>
+            <Grid size={12} padding={"0 1rem"}>
+              <small>{task.description}</small>
+            </Grid>
+            <Grid size={12} padding={"0 1rem"}>
+              <p>{task.state}</p>
+            </Grid>
+
+            <Grid size={6} offset={6}>
+              <Grid container>
+                <Grid size={6}>
+                  <Button>Edit</Button>
+                </Grid>
+
+                <Grid size={6}>
+                  <Button>Delete</Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
     </>
   );
 }
