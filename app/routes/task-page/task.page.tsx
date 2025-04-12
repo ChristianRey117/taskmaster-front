4c75125a1c02f60939eac13d1a8f6f0bc0bbe80f
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2025-04-12 00:16:11 -0400

    fix(task-page): add provider to page

--- app/routes/task-page/task.page.tsx
+++ app/routes/task-page/task.page.tsx
@@ -1,9 +1,11 @@
+import { Provider } from "react-redux";
 import TaskList from "~/components/TaskList/task-list";
+import store from "~/redux/store";
 
 export default function TaskPage() {
   return (
-    <>
+    <Provider store={store}>
       <TaskList></TaskList>
-    </>
+    </Provider>
   );
 }
