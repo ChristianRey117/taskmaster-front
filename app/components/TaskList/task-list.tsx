9e62305c59a754e86f17ce4eceebee4631527798
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2025-04-11 22:55:19 -0400

    feat(task-list): use filter with state

--- app/components/TaskList/task-list.tsx
+++ app/components/TaskList/task-list.tsx
@@ -1,10 +1,13 @@
-import { Grid } from "@mui/material";
+import { Autocomplete, Grid, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 import type { ITask, ITaskResponse } from "~/intefaces/ITaskResponse";
 import Task from "./Task/task";
 
 export default function TaskList() {
   const [tasks, setTasks] = useState<ITask[]>([]);
+  const [statusTask, setStatusTask] = useState<
+    "TODO" | "DOING" | "DONE" | null
+  >(null);
 
   useEffect(() => {
     getData();
@@ -25,11 +28,46 @@ export default function TaskList() {
 
       <Grid container size={12}>
         <Grid size={6}>
-          <p>Filtro</p>
+          <p>Filter</p>
         </Grid>
-        <Grid size={6}>Dropdown</Grid>
+        <Grid size={6}>
+          <Autocomplete
+            disablePortal
+            options={["TODO", "DOING", "DONE"]}
+            sx={{ width: 300 }}
+            onChange={(e) => {
+              setStatusTask(
+                e.currentTarget.textContent as "TODO" | "DONE" | "DOING"
+              );
+            }}
+            renderInput={(params) => (
+              <TextField {...params} label="State Task" />
+            )}
+          />
+        </Grid>
       </Grid>
       {tasks.map((task) => {
+        if (statusTask) {
+          if (statusTask == task.state) {
+            return (
+              <Grid
+                size={{ xs: 12, sm: 12, md: 6, lg: 4, xl: 3 }}
+                padding={"2rem"}
+                rowSpacing={3}
+                key={task.id}
+              >
+                <Task
+                  key={task.id}
+                  id={task.id}
+                  description={task.description}
+                  state={task.state}
+                  title={task.title}
+                ></Task>
+              </Grid>
+            );
+          }
+          return;
+        }
         return (
           <Grid
             size={{ xs: 12, sm: 12, md: 6, lg: 4, xl: 3 }}
